0913ef4ad035365b727b8262c5610cdfe180fbe9
Andrea Mei Maala <108439638+meisprout@users.noreply.github.com>
2024-08-11 21:33:26 +0800

    added cv

--- dev-portfolio-site/src/app/components/nav.tsx
+++ dev-portfolio-site/src/app/components/nav.tsx
@@ -23,11 +23,24 @@ export default function Nav() {
         />
       </Link>
 
-      <div onClick={openNav} className={`${styles["sidenav-button"]} d-lg-none d-block`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-        </svg>
+      <div className={`d-lg-none d-flex align-items-center gap-3`}>
+        <Link href="/files/ANDREA_MEI_MAALA_CV.pdf" target="_blank" onClick={closeNav}>
+          <button className={`btn`}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-text" viewBox="0 0 16 16">
+              <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z" />
+              <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
+            </svg> View CV
+          </button>
+        </Link>
+
+        <div onClick={openNav} className={`${styles["sidenav-button"]} `}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+        </div>
       </div>
+
+      
       <ul className={`list-unstyled ${styles.sidenav} ${isOpen ? `${styles.open}` : `${styles.closed}`}`}>
         
         <li>
@@ -51,16 +64,6 @@ export default function Nav() {
         <li>
           <Link className={"text-decoration-none"} href="./#contact" onClick={closeNav}>
             Contact
-          </Link>
-        </li>
-        <li>
-          <Link href="/files/ANDREA_MEI_MAALA_CV.pdf" target="_blank" onClick={closeNav}>
-            <button className={`btn button-alt`}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-text" viewBox="0 0 16 16">
-                <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5M5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z" />
-                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1" />
-              </svg> View CV
-            </button>
           </Link>
         </li>
       </ul>
