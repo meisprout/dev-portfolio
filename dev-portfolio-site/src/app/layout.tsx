41a3cf6ae67c841d30e5ae227e514d5151aa0112
Andrea Mei Maala <108439638+meisprout@users.noreply.github.com>
2024-08-11 21:29:30 +0800

    v2 done

--- dev-portfolio-site/src/app/layout.tsx
+++ dev-portfolio-site/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import './styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Meisprout Web Developer Portfolio',
-  description: 'A portfolio for a web designer and developer using NextJS.',
+  title: 'Eah Maala Web Developer Portfolio',
+  description: 'Front-end Developer and Web/UI Designer Portfolio. Made in NextJS Typescript.',
 }
 
 export default function RootLayout({
@@ -16,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
 
       <Head>
-        <title>Meisprout Web Developer Portfolio</title>
+        <title>Eah Maala Web Developer Portfolio</title>
         <meta name="description" content="Portfolio of a web developer from the Philippines" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
